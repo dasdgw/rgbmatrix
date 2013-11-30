ef90ca4a2b449b36188bae15795b51035fa8c526
dasdgw <michifrank2@gmx.de>
2013-11-30 01:08:07 +0100

    fix and enhance rgbmatrix pixel position tcl test script

--- qsys/system_console.tcl
+++ qsys/system_console.tcl
@@ -1,6 +1,7 @@
-set sysid_base 0x0
+set sysid_base 0x10000
 set rgbmatrix_base 0x2000
 set rgbmatrix_size 0x2000
+set hps_peripherals_base 0xFF200000
 
 set rgbmatrix_cols 32
 set rgbmatrix_rows 16
@@ -69,34 +70,42 @@ proc corners {} {
     global rgbmatrix_rows
     global pixel_width
 
-    master_write_32  $mypath 0x2004 0x0000EF00
-    #0,0
-#    master_write_32  $mypath $rgbmatrix_base 0x000000EF
-    
-    #31,0
-#   set address [expr {$rgbmatrix_base + [expr{[expr{$rgbmatrix_cols -1}] * $pixel_width}]} ]
-#   master_write_32  $mypath $address 0x0000EF00
-#
-#   #0,15
-#   set address [expr {$rgbmatrix_base + [expr{[expr{$rgbmatrix_cols * [expr{$rgbmatrix_rows-1}]}] * $pixel_width}]} ]
-#   master_write_32  $mypath $address 0x00EF0000
-#   
-#   #31,15
-#   set address [expr {$rgbmatrix_base + [expr{[expr{ [expr{$rgbmatrix_cols *$rgbmatrix_rows}]}-1] * $pixel_width}]} ]
-#et address [expr {$rgbmatrix_base + $rgbmatrix_size -1} ]
-#   master_write_32  $mypath $address 0x0000EFEF
-
-#    for {set x 0} {$x<$rgbmatrix_size} {incr x $pixel_width} {
-      #    }
+    put_pixel_hps 0 0
+    put_pixel_hps 0 7
+    put_pixel_hps 0 8
+    put_pixel_hps 0 15
+    put_pixel_hps 31 0
+    put_pixel_hps 31 7
+    put_pixel_hps 31 8
+    put_pixel_hps 31 15
 }
 
-proc put_pixel {x,y} {
+proc put_pixel {x y} {
     global mypath
     global rgbmatrix_base
     global rgbmatrix_size
+    global rgbmatrix_cols
     global pixel_width
 
-    set address [expr {$rgbmatrix_base + [expr{ [expr{ $x + [expr{$rgbmatrix_cols*$y} ]}] * $pixel_width}]} ]
+#    set address_x $x
+#    set address_y [expr {32*$y} ]
+    set address [expr {$rgbmatrix_base + [expr { [expr { $x + [expr {$rgbmatrix_cols * $y} ]}] * $pixel_width}]} ]
+    master_write_32  $mypath $address 0x00EF0000
+
+}
+
+
+proc put_pixel_hps {x y} {
+    global mypath
+    global rgbmatrix_base
+    global rgbmatrix_size
+    global rgbmatrix_cols
+    global pixel_width
+    global hps_peripherals_base
+
+#    set address_x $x
+#    set address_y [expr {32*$y} ]
+    set address [expr {$hps_peripherals_base + $rgbmatrix_base + [expr { [expr { $x + [expr {$rgbmatrix_cols * $y} ]}] * $pixel_width}]} ]
     master_write_32  $mypath $address 0x00EF0000
 
 }
